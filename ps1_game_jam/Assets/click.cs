fd3fa67ec03ec19c5ad49d8a629c499e5b4e8128
birdboiboi <jai36@drexel.edu>
2020-11-15 05:46:36 -0500

    added title screen that loads scene

---
+++ ps1_game_jam/Assets/click.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class click : MonoBehaviour
+{
+    private SpriteRenderer rend;
+    public string cmd;
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+    }
+  
+   
+    void OnMouseExit()
+    {
+        
+        rend.color = Color.white;
+    }
+    void OnMouseDown()
+    {
+        rend.color = Color.blue;
+        Debug.Log("howdy");
+        ExecScrpt(cmd);
+    }
+    void OnMouseUp()
+    { 
+        OnMouseExit();
+    }
+    void ExecScrpt(string cmd)
+    {
+        switch (cmd)
+        {
+            case "OpenScene":
+                OpenScene();
+                break;
+            case "EndGame":
+                EndGame();
+                break;
+        }
+    }
+
+    public void OpenScene()
+    {
+        //SceneManager.LoadScene("play_test");
+        SceneManager.LoadScene("play_test");
+    }
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+
+}
+
+
+
---
+++ ps1_game_jam/Assets/sway.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sway : MonoBehaviour
+{
+    public float swaySpeed = .00001f;
+    public float swayRadius = .00000000001f;
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(swayRadius * Mathf.Sin(Time.time), swayRadius* Mathf.Cos(Time.time)));
+    }
+}
