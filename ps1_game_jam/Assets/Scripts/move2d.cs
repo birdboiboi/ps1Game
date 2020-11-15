5ce4348d1aed9930501bff5da330214b40611b35
birdboiboi <jai36@drexel.edu>
2020-11-15 02:47:29 -0500

    multiplle wall jumps in and other stuff like camera switching views and parents

--- ps1_game_jam/Assets/Scripts/move2d.cs
+++ ps1_game_jam/Assets/Scripts/move2d.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class move2d : MonoBehaviour
 {
     public float speed;                //Floating point variable to store the player's movement speed.
-
-    //private Rigidbody rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    public bool jump;
+    //private Rigidbody rb;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
 
     // Use this for initialization
     void Start()
     {
         //Get and store a reference to the Rigidbody2D component so that we can access it.
-        //rb2d = GetComponent<Rigidbody>();
+       // rb = GetComponent<Rigidbody>();
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
@@ -20,16 +20,19 @@ public class move2d : MonoBehaviour
     {
        
         //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveHorizontal = -Input.GetAxis("Horizontal");
 
         //Store the current vertical input in the float moveVertical.
         float moveVertical = Input.GetAxis("Vertical");
-
+        if (moveVertical > 0)
+        {
+            moveVertical = 1;
+        }
         //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector3(moveHorizontal, moveVertical,0);
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical,0);
         
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        //rb2d.AddForce(movement * speed);
+        //rb.AddForce(movement * speed);
         transform.Translate(movement * speed);
     }
 }
--- ps1_game_jam/Assets/WallManager.cs
+++ ps1_game_jam/Assets/WallManager.cs
@@ -5,26 +5,51 @@ using UnityEngine;
 public class WallManager : MonoBehaviour
 {
     public GameObject mainCamera;
-    public SpriteRenderer player;
+    public SpriteRenderer player2d;
+    public GameObject player3d;
+    private Vector3 contraint;
+    private bool inWall = false;
     // Start is called before the first frame update
     void Start()
     {
-        player.enabled = false;
+        player2d.enabled = false;
+        contraint = new Vector3(1,1,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButton("Jump")   &&  inWall )
+        {
+            player2d.enabled = false;
+            player3d.SetActive(true);
+            player3d.transform.position = player2d.transform.position + Vector3.up  ;
+            //player3d.transform.rotation = player2d.transform.rotation;
+            //player3d.transform.Rotate(0, 90, 0);
+            
+            inWall = false;
+            mainCamera.GetComponent<Camera_Follow>().enabled = true;
+            mainCamera.transform.parent = null;
+
+        }
     }
 
     void OnTriggerEnter(Collider col)
     {
         Debug.Log("enter");
-        if (col.tag == "Player")
+        if (col.tag == player3d.tag)
         {
-            Destroy(col.gameObject);
-            player.enabled = true;
+            
+            col.gameObject.SetActive(false);
+
+            player2d.transform.position = Vector3.Scale(player3d.transform.position , contraint);
+            player2d.transform.eulerAngles = transform.eulerAngles + new Vector3(90,0,0);
+            player2d.enabled = true;
+            mainCamera.transform.position = player2d.transform.position + player2d.transform.forward * 5;
+            mainCamera.GetComponent<Camera_Follow>().enabled = false;
+            mainCamera.transform.LookAt(player2d.transform);
+            mainCamera.transform.parent = player2d.transform;
+            inWall = true;
         }
     }
 }
